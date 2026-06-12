68884386c4afdd92693afd31bda229a58899db8d
Xuqing <evan060893@gmail.com>
2026-06-12 22:21:36 +0800

    更新 .gitignore

--- CSharp/obj/Debug/BaroWardrobeSwitcher.AssemblyInfo.cs
+++ CSharp/obj/Debug/BaroWardrobeSwitcher.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("BaroWardrobeSwitcher")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7222839f58a9bd643ee010ef64aa168497a98f88")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c39cefc1b328833103c188cbb9a95c023cea087d")]
 [assembly: System.Reflection.AssemblyProductAttribute("BaroWardrobeSwitcher")]
 [assembly: System.Reflection.AssemblyTitleAttribute("BaroWardrobeSwitcher")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
